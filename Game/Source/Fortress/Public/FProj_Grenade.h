5dca000bbdaf770a2077e5c9573a962c13ed79f5
MichaelRDavis <MichaelRDavis1991@protonmail.com>
2019-10-15 13:48:08 +0100

    Added basic building machanics

---
+++ Game/Source/Fortress/Public/FProj_Grenade.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FProjectile.h"
+#include "FProj_Grenade.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FORTRESS_API AFProj_Grenade : public AFProjectile
+{
+	GENERATED_BODY()
+	
+};
