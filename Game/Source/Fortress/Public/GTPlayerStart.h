db404d4b756a95a3c5a5f46c731aefe9d795066e
MichaelRDavis <MichaelRDavis1991@protonmail.com>
2019-11-01 16:22:25 +0000

    Added team starts

--- Game/Source/Fortress/Public/GTPlayerStart.h
+++ Game/Source/Fortress/Public/GTPlayerStart.h
@@ -6,12 +6,15 @@
 #include "GameFramework/PlayerStart.h"
 #include "GTPlayerStart.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class FORTRESS_API AGTPlayerStart : public APlayerStart
 {
 	GENERATED_BODY()
 	
+public:
+	AGTPlayerStart(const FObjectInitializer& ObjectInitializer);
+
+	/** Which team can spawn at this point */
+	UPROPERTY(EditInstanceOnly, Category = Team)
+	int32 Team;
 };
