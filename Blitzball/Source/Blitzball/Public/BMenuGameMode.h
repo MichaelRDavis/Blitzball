ab19acbd0c9125b919f8f8693b139a1b365f4f31
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-11-14 14:11:52 +0000

    Removed large files

---
+++ Blitzball/Source/Blitzball/Public/BMenuGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BGameMode.h"
+#include "BMenuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLITZBALL_API ABMenuGameMode : public ABGameMode
+{
+	GENERATED_BODY()
+	
+};
